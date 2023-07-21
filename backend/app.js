ebcc0f128f772b1b30f12fa57df297da72d226ca
Anatoliy Sannikov <WildJuk@yandex.ru>
2023-07-21 10:28:56 +0500

    PR №15

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const UnknowErr = require('./middlewares/unknow-err');
 const router = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { DATA_BASE_URI, PORT } = require('./config');
 
 const app = express();
@@ -14,7 +15,17 @@ mongoose.connect(DATA_BASE_URI);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 app.use(UnknowErr);
 app.listen(PORT, () => console.log('Сервер запущен'));
